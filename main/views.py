10f85a4c0d8b686070a8655f87c4c1add6ff2e1b
Ho <cho@kor1.onmicrosoft.com>
2020-02-17 12:53:56 +0900

    first commit2

---
+++ main/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def index(request):
+
+    n = list(range(1,46))
+    gamesets = []
+    
+    while len(n) >= 3:
+        gameset=[]
+        while len(gameset) <6:
+    
+            if len(n) > 3:
+                i = random.randrange(0,len(n))
+                gameset.append(n[i])
+                n.pop(i)
+              
+        gameset.sort()
+        gamesets.append(gameset)
+        print(gameset)
+    
+        if len(n) == 3:
+    
+            new_n = list(set(range(1,46)) - set(n))     
+    
+            for i in range(0,3):
+                j = random.randrange(1,len(new_n))
+                n.append(new_n[j])
+                new_n.pop(j)
+    
+    
+            print(n)
+            gamesets.append(n)
+    
+    
+            break
+        outputlist =[]
+        for game in gamesets:
+            outputlist.append(str(game))
+    
+    outputs = '<h1>Good Luck!</h1>'+'</br>'.join(outputlist)
+
+    return HttpResponse(outputs)

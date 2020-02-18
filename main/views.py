a4dc141f12012f52c1d63faa9c964359fd102d80
Ho <cho@kor1.onmicrosoft.com>
2020-02-18 16:22:26 +0900

    add exemption

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 import random
 
 # Create your views here.
 
+@xframe_options_exempt
 def index(request):
 
     n = list(range(1,46))
